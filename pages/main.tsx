a4cd8585a3c8147c03c8f6468dafe6331912fb2d
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-02-01 11:23:50 +0100

    Made the main page(accesible after login)

--- pages/main.tsx
+++ pages/main.tsx
@@ -5,6 +5,8 @@ import app from "../firebase/clientApp";
 import style from "../styles/main.module.css";
 import { db } from "../firebase/clientApp";
 import { doc, getDoc } from "firebase/firestore";
+import SearchBar from "../components/searchBar";
+import Item from "../components/item";
 
 var currentUser: User | null;
 var signedIn: boolean;
@@ -12,7 +14,8 @@ var name: string | null;
 
 const auth = getAuth(app);
 
-const main: NextPage = (props) => {
+const Main: NextPage = (props) => {
+  const { songs, albums, playlist }: any = props;
   const [user, setUser]: any = useState("Person");
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -28,20 +31,35 @@ const main: NextPage = (props) => {
 
   return (
     <div className={style.outer}>
-      <p className={style.text}>Hello {user}</p>
-      <button
-        type="button"
-        className={style.button}
-        onClick={() =>
-          signOut(auth).then(
-            () => (window.location.href = "http://localhost:3000/login")
-          )
-        }
-      >
-        Log out
-      </button>
+      <div className={style.navbar}>
+        <p className={style.org}>Primal Tech</p>
+        <div className={style.search__box}>
+          <input className={style.search__input} />
+          <button className={style.search__button}>search</button>
+        </div>
+      </div>
+      <div className={style.song__section}>
+        <div className={style.list}>
+          <p className={style.list__name}>Top Tracks</p>
+          <div className={style.items__container}>
+            {/*Item component goes here*/}
+          </div>
+        </div>
+        <div className={style.list}>
+          <p className={style.list__name}>Top Albums</p>
+          <div className={style.items__container}>
+            {/*Item component goes here*/}
+          </div>
+        </div>
+        <div className={style.list}>
+          <p className={style.list__name}>My Playlist</p>
+          <div className={style.items__container}>
+            {/*Item component goes here*/}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default main;
+export default Main;
