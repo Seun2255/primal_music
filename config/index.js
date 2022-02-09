42d663c41309f2c6950913110631f9873a6444ac
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-02-09 20:39:32 +0100

    Added proxy url to config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
 const searchTrack =
-  "https://cryptic-headland-94862.herokuapp.com/https://api.deezer.com/search?q=";
-const topTracks = "https://api.deezer.com/chart/0/tracks";
-const topAlbums = "https://api.deezer.com/chart/0/albums";
+  "https://sleepy-everglades-42596.herokuapp.com/https://api.deezer.com/search?q=";
+const topTracks =
+  "https://sleepy-everglades-42596.herokuapp.com/https://api.deezer.com/chart/0/tracks";
+const topAlbums =
+  "https://sleepy-everglades-42596.herokuapp.com/https://api.deezer.com/chart/0/albums";
 
 const urls = {
   search: searchTrack,
