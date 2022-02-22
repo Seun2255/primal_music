2867bed3c6e5baa7a1495aa61e7114cef2b6902a
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-02-22 15:45:31 +0100

    Made huge changes and added features to the search bar component

--- components/searchBar.js
+++ components/searchBar.js
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import style from "../styles/searchBar.module.css";
 import { Search } from "@mui/icons-material";
 import SearchItem from "./searchItem";
-import search from "../pages/api/deezerAPI";
+import deezerAPI from "../pages/api/deezerAPI";
+import SongView from "./songView";
+import { Bars } from "react-loader-spinner";
 
 function SearchBar(props) {
   const [input, setInput] = useState("");
   const [result, setResult] = useState(false);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(["test", 5]);
+  const [showsong, setShowsong] = useState(false);
+  const [selectedSong, setSelectedSong] = useState({});
+  const [loader, setLoader] = useState(false);
+
+  const displaySong = (song) => {
+    setSelectedSong(song);
+    setShowsong(true);
+  };
 
   return (
-    <div className={style.outer}>
+    <div className={style.outer} id={result ? style.focus : "null"}>
       <div className={style.search__box}>
         <input
           type="search"
           placeholder="Search"
           className={style.search__input}
+          id={result ? style.input__focus : "null"}
           onChange={(e) => {
             setInput(e.target.value);
             if (input === "") {
@@ -25,22 +36,50 @@ function SearchBar(props) {
         />
         <button
           className={style.search__button}
-          onClick={() => {
-            setData(search(input));
+          onClick={async () => {
             if (input !== "") {
+              setLoader(true);
               setResult(true);
+              setData(await deezerAPI.search(input));
+              setLoader(false);
             }
           }}
         >
-          search
+          <Search
+            sx={{ color: "white" }}
+            style={{ width: "100%", height: "100%" }}
+          />
         </button>
       </div>
-      {result && (
-        <div className={style.search__results}>
-          {data.map((item) => {
-            <SearchItem song={item.title_short} artist={item.artist.name} />;
-          })}
-        </div>
+      {result &&
+        (loader ? (
+          <div className={style.loader}>
+            <Bars color="#8a2be2" height={140} width={140} />
+          </div>
+        ) : (
+          <div
+            className={style.search__results}
+            onBlur={() => setResult(false)}
+          >
+            {data.map((item) => {
+              return (
+                <SearchItem
+                  song={item.title_short}
+                  artist={item.artist.name}
+                  onClick={() => {
+                    displaySong(item);
+                  }}
+                />
+              );
+            })}
+          </div>
+        ))}
+      {showsong && (
+        <SongView
+          song={selectedSong}
+          closeModal={() => setShowsong(false)}
+          page="home"
+        />
       )}
     </div>
   );
--- components/searchItem.js
+++ components/searchItem.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import style from "../styles/searchItem.module.css";
 
-function SearchItem(props, { params }) {
-  const { song, artist } = { params };
+function SearchItem(props) {
+  const { song, artist, onClick } = props;
 
   return (
-    <div className={style.outer}>
+    <div
+      className={style.outer}
+      onClick={() => {
+        console.log("Should be working");
+        onClick();
+      }}
+    >
       <p className={style.song}>{song}</p>
       <p className={style.artist}>{artist}</p>
     </div>
