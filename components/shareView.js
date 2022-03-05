67cd1aceeb37e37b80f6435b2f14ff81407669cd
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-03-05 15:00:34 +0100

    Changed the feature ofr sharing playlist

--- components/shareView.js
+++ components/shareView.js
@@ -3,53 +3,22 @@ import style from "../styles/shareView.module.css";
 import Image from "next/image";
 import cross from "../assets/cross.png";
 import { async } from "@firebase/util";
-import { Button } from "@mui/material";
-import { Share } from "@mui/icons-material";
-import { Modal } from "react-responsive-modal";
-import { send } from "emailjs-com";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Share from "@mui/icons-material/Share";
+import Whatsapp from "@mui/icons-material/WhatsApp";
+import Gmail from "@mui/icons-material/Email";
 
 function ShareView(props) {
   const { playlist, closeModal, name } = props;
-  const [mode, setMode] = useState("Email");
+  const [mode, setMode] = useState("Whatsapp");
   const [email, setEmail] = useState("");
-  const [number, setNumber] = useState("+234");
-  const [open, setOpen] = useState(false);
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
-  const [open2, setOpen2] = useState(false);
-  const onOpenModal2 = () => setOpen2(true);
-  const onCloseModal2 = () => setOpen2(false);
 
   var message;
   playlist.tracks.map((item) => {
     var text = item.title_short + " - " + item.artist.name + "\n";
     message += text;
   });
-
-  const submit = async () => {
-    const data = {
-      from_name: name,
-      to_name: "",
-      message: message,
-      reply_to: email,
-    };
-    send(
-      process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
-      "template_onyx7dk",
-      data,
-      "DVWjfHmYNWHWfqrQ5"
-    )
-      .then((response) => {
-        onOpenModal();
-        console.log("SUCCESS!", response.status, response.text);
-        setTimeout(() => {
-          onCloseModal();
-        }, 2000);
-      })
-      .catch((err) => {
-        console.log("FAILED...", err);
-      });
-  };
 
   return (
     <div className={style.modal}>
@@ -65,33 +34,37 @@ function ShareView(props) {
           />
         </div>
         <div className={style.switch}>
-          <Button
-            variant="contained"
-            style={{
-              backgroundColor: "red",
-              color: "white",
-              cursor: "pointer",
-              marginRight: "4px",
-            }}
-            onClick={() => {
-              setMode("Email");
-            }}
-          >
-            Email
-          </Button>
-          <Button
-            variant="contained"
-            style={{
-              backgroundColor: "green",
-              color: "white",
-              cursor: "pointer",
-            }}
-            onClick={() => {
-              setMode("Whatsapp");
-            }}
-          >
-            Whatsapp
-          </Button>
+          <ButtonGroup>
+            <Button
+              startIcon={<Whatsapp />}
+              variant="contained"
+              style={{
+                backgroundColor: "#8a2be2",
+                color: "white",
+                cursor: "pointer",
+                marginRight: "4px",
+              }}
+              onClick={() => {
+                setMode("Whatsapp");
+              }}
+            >
+              Whatsapp
+            </Button>
+            <Button
+              startIcon={<Gmail />}
+              variant="contained"
+              style={{
+                backgroundColor: "#8a2be2",
+                color: "white",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                setMode("Email");
+              }}
+            >
+              Email
+            </Button>
+          </ButtonGroup>
         </div>
         {mode == "Email" ? (
           <div className={style.container}>
@@ -103,73 +76,37 @@ function ShareView(props) {
               ></input>
             </div>
             <Button
-              startIcon={
-                <Share style={{ color: "white", backgroundColor: "#8a2be2" }} />
-              }
+              startIcon={<Share />}
               variant="contained"
               style={{
                 backgroundColor: "#8a2be2",
                 color: "white",
                 cursor: "pointer",
               }}
-              onClick={() => {
-                // page == "home" ? onOpenModal() : addSong();
-              }}
             >
-              Share
+              <a
+                href={`mailto:${email}?subject=Check out this playlist Subject&body=${message}`}
+              >
+                Share
+              </a>
             </Button>
           </div>
         ) : (
           <div className={style.container}>
-            <div className={style.item}>
-              <p className={style.item__name}>Number</p>
-              <input
-                className={style.item__input}
-                onChange={(e) => setNumber(e.target.value)}
-                value={number}
-              />
-            </div>
             <Button
-              startIcon={
-                <Share style={{ color: "white", backgroundColor: "#8a2be2" }} />
-              }
+              startIcon={<Share />}
               variant="contained"
               style={{
                 backgroundColor: "#8a2be2",
                 color: "white",
                 cursor: "pointer",
               }}
-              onClick={() => {
-                // page == "home" ? onOpenModal() : addSong();
-              }}
             >
-              Share
+              <a href={`https://wa.me/?text=${message}`}>Share</a>
             </Button>
           </div>
         )}
       </div>
-      <Modal
-        open={open}
-        onClose={onCloseModal}
-        center
-        classNames={{
-          overlay: style.customOverlay,
-          modal: style.customModal,
-        }}
-      >
-        <h2>Playlist sent</h2>
-      </Modal>
-      <Modal
-        open={open2}
-        onClose={onCloseModal2}
-        center
-        classNames={{
-          overlay: style.customOverlay2,
-          modal: style.customModal2,
-        }}
-      >
-        <h2>Failed, please try again</h2>
-      </Modal>
     </div>
   );
 }
